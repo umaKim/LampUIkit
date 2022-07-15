f53c6aae9761a14d4f5acfde05d8431bd6eb61d6
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-15 20:14:09 +0900

    chore: add firebase pod file

---
+++ LampUIKit/Pods/Target Support Files/Pods-LampUIKit/Pods-LampUIKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LampUIKit : NSObject
+@end
+@implementation PodsDummy_Pods_LampUIKit
+@end
---
+++ LampUIKit/Pods/Target Support Files/Pods-LampUIKitTests/Pods-LampUIKitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LampUIKitTests : NSObject
+@end
+@implementation PodsDummy_Pods_LampUIKitTests
+@end
