82168bf82bd6f959427a11aa9956e11d386ad474
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-17 22:30:38 +0900

    feat: set up custom present method for vc

--- LampUIKit/LampUIKit/Helper/Extensions.swift
+++ LampUIKit/LampUIKit/Helper/Extensions.swift
@@ -32,3 +32,20 @@ extension UIImage {
     static let gear = UIImage(systemName: "gear")
 }
 
+extension UIViewController {
+    open func present(_ viewControllerToPresent: UIViewController, transitionType: CATransitionSubtype = .fromRight, animated flag: Bool, pushing: Bool, completion: (() -> Void)? = nil) {
+        if pushing {
+            let transition = CATransition()
+            transition.duration = 0.5
+            transition.type = CATransitionType.push
+            transition.subtype = transitionType
+            transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+            view.window?.layer.add(transition, forKey: kCATransition)
+            viewControllerToPresent.modalPresentationStyle = .fullScreen
+            self.present(viewControllerToPresent, animated: false, completion: completion)
+            
+        } else {
+            self.present(viewControllerToPresent, animated: flag, completion: completion)
+        }
+    }
+}
