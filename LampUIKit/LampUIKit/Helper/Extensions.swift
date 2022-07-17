7d0da61c9079d6a5cdd4cb931ad6ec635d6a552f
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-17 22:29:49 +0900

    feat: add new colors

--- LampUIKit/LampUIKit/Helper/Extensions.swift
+++ LampUIKit/LampUIKit/Helper/Extensions.swift
@@ -18,6 +18,8 @@ extension UIScreen {
 }
 
 extension UIColor {
+    static let midNavy = UIColor(red: 92/255, green: 92/255, blue: 141/255, alpha: 1)
+    static let lightGrey = UIColor(red: 228/255, green: 228/255, blue: 232/255, alpha: 1)
     static let greyshWhite = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1)
     static let darkNavy = UIColor(red: 38/255, green: 38/255, blue: 92/255, alpha: 1)
     static let lightNavy = UIColor(red: 112/255, green: 112/255, blue: 255/255, alpha: 1)
