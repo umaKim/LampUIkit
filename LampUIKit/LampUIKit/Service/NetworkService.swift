cb3a2dd126a62f19d3f7a2afb9c64d3832cf7e17
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-17 22:28:54 +0900

    feat: set initial set up for network Service

---
+++ LampUIKit/LampUIKit/Service/NetworkService.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkService.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/07/17.
+//
+
+import Foundation
+
+class NetworkService {
+    static let shared = NetworkService()
+    
+    func fetch() {
+        
+    }
+}
