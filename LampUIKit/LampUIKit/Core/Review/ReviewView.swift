a800f26a8aa5cd1d322941e70cd76818d47da47d
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-22 08:29:30 +0900

    feat: initial set up for Review page

---
+++ LampUIKit/LampUIKit/Core/Review/ReviewView.swift
@@ -0,0 +1,20 @@
+//
+//  ReviewView.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/07/21.
+//
+
+import UIKit
+
+class ReviewView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
