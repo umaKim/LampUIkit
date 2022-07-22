8d09113d62c45564db183fa92a4b9cba1a6d0e3b
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-23 02:27:29 +0900

    feat: ui set up

--- LampUIKit/LampUIKit/Core/LampSpot/Views/MyMapCollectionViewCell.swift
+++ LampUIKit/LampUIKit/Core/LampSpot/Views/MyMapCollectionViewCell.swift
@@ -1,14 +1,88 @@
+//
+//  MyMapCollectionViewCell.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/07/14.
+//
+
+import UIKit
+
 protocol MyMapCollectionViewCellDelegate: AnyObject {
     func didSelectMyLampImage()
 }
 
+class MyMapCollectionViewCell: UICollectionViewCell {
+    static let identifier = "MyMapCollectionViewCell"
     
     weak var delegate: MyMapCollectionViewCellDelegate?
+    
+    private lazy var titleLabel: UILabel = {
+       let lb = UILabel()
+        lb.text = "MY LAMP"
+        lb.font = .systemFont(ofSize: 28, weight: .bold)
+        lb.textColor = .darkNavy
+        return lb
+    }()
+    
+    private lazy var subTitleLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "대한민국 지도에 나만의 흔적 남기기"
+        lb.font = .systemFont(ofSize: 16, weight: .bold)
+        lb.textColor = .lightNavy
+        return lb
+    }()
+    
+    private lazy var mapImageView: UIImageView = {
+        let image = UIImage(named: "myLampMap")
+        let uv = UIImageView(image: image)
+        uv.layer.cornerRadius = 16
+        uv.contentMode = .scaleAspectFit
+        uv.layer.masksToBounds = true
+        uv.isUserInteractionEnabled = true
+        return uv
+    }()
+    
     @objc
     private func didTap() {
         print("didTap")
         delegate?.didSelectMyLampImage()
     }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .greyshWhite
         
         let gesture = UITapGestureRecognizer(target: self, action: #selector(didTap))
         mapImageView.addGestureRecognizer(gesture)
+        
+        let labelStackView = UIStackView(arrangedSubviews: [titleLabel, subTitleLabel])
+        labelStackView.axis = .vertical
+        labelStackView.alignment = .leading
+        labelStackView.distribution = .fill
+        labelStackView.spacing = 6
+        
+        let totalStackView = UIStackView(arrangedSubviews: [labelStackView, mapImageView])
+        totalStackView.axis = .vertical
+        totalStackView.alignment = .fill
+        totalStackView.distribution = .fill
+        totalStackView.spacing = 17
+        
+        [totalStackView].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(uv)
+        }
+        
+        NSLayoutConstraint.activate([
+            labelStackView.heightAnchor.constraint(equalToConstant: 60),
+            
+            totalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            totalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            totalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            totalStackView.topAnchor.constraint(equalTo: contentView.topAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- LampUIKit/LampUIKit/Core/LampSpot/Views/PopularLampSpotCollectionViewCell.swift
+++ LampUIKit/LampUIKit/Core/LampSpot/Views/PopularLampSpotCollectionViewCell.swift
@@ -1,13 +1,71 @@
+//
+//  PopularLampSpotCollectionViewCell.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/07/14.
+//
+
+import UIKit
 
 protocol PopularLampSpotCollectionViewCellDelegate:AnyObject {
     func popularLampSpotCollectionViewCellDidTap()
 }
 
+class PopularLampSpotCollectionViewCell: UICollectionViewCell {
+    static let identifier = "PopularLampSpotCollectionViewCell"
     
     weak var delegate: PopularLampSpotCollectionViewCellDelegate?
+    
+    private let titleLabel:UILabel = {
+       let lb = UILabel()
+        lb.text = "지금 인기 있는 램프 스팟"
+        lb.font = .systemFont(ofSize: 20, weight: .semibold)
+        lb.textColor = .black
+        return lb
+    }()
+    
+    private lazy var mapImageView: UIImageView = {
+       let uv = UIImageView(image: UIImage(systemName: "house"))
+        uv.layer.cornerRadius = 16
+        uv.backgroundColor = .systemBrown
+        uv.isUserInteractionEnabled = true
+        return uv
+    }()
+    
     @objc
     private func tapHandler() {
         delegate?.popularLampSpotCollectionViewCellDidTap()
     }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .greyshWhite
+        
         let tap = UITapGestureRecognizer(target: self, action: #selector(tapHandler))
         mapImageView.addGestureRecognizer(tap)
+        
+        let labelStackView = UIStackView(arrangedSubviews: [titleLabel, mapImageView])
+        labelStackView.axis = .vertical
+        labelStackView.alignment = .fill
+        labelStackView.distribution = .fill
+        labelStackView.spacing = 17
+        
+        [labelStackView].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(uv)
+        }
+        
+        NSLayoutConstraint.activate([
+            titleLabel.heightAnchor.constraint(equalToConstant: 24),
+            
+            labelStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            labelStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            labelStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            labelStackView.topAnchor.constraint(equalTo: contentView.topAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
