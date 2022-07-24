02837e95e7c020d6015e69b3ca1417d519c6d0ef
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-24 22:54:09 +0900

    chore: set up LampSpotResponse model

--- LampUIKit/LampUIKit/Core/LampSpot/LampSpotViewModel.swift
+++ LampUIKit/LampUIKit/Core/LampSpot/LampSpotViewModel.swift
@@ -4,9 +4,14 @@
 //
 //  Created by 김윤석 on 2022/07/13.
 //
-
+import UIKit
 import Foundation
 
 class LampSpotViewModel {
     private(set) var models: [String] = ["model1", "model2", "model3"]
+}
+
+struct LampSpotResponse: Decodable {
+    let recomendingPlaces: [LocationItem]
+    let popularPlace: LocationItem
 }
