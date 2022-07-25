b6bb59ed8e86319a6c9fe3019706492151f7f138
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-26 02:00:14 +0900

    chore: set up EvaluationCollectionView Cell

--- LampUIKit/LampUIKit/Core/WriteReview/Views/EvaluationCollectionViewCell.swift
+++ LampUIKit/LampUIKit/Core/WriteReview/Views/EvaluationCollectionViewCell.swift
@@ -1,10 +1,34 @@
+//
+//  EvaluationCollectionViewCell.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/07/20.
+//
+
+import UIKit
+
+final class EvaluationCollectionViewCell: UICollectionViewCell {
+    static let identifier = "EvaluationCollectionViewCell"
+    
+    static let preferredHeight: CGFloat = 30
+    
     private lazy var titleLabel: UILabel = {
         let lb = UILabel()
         lb.textColor = .black
         lb.font = .systemFont(ofSize: 15, weight: .semibold)
         return lb
     }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.cornerRadius = EvaluationCollectionViewCell.preferredHeight / 2
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.red.cgColor
+        
         setupUI()
+    }
+    
     private func setupUI() {
         [titleLabel].forEach { uv in
             uv.translatesAutoresizingMaskIntoConstraints = false
@@ -18,6 +42,10 @@
         ])
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     public func configure(with model: EvaluationModel) {
         titleLabel.text = model.title
         
@@ -29,3 +57,4 @@
             self.titleLabel.textColor = .systemGray
         }
     }
+}
