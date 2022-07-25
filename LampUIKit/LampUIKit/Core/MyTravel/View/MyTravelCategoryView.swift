3179eb9259956d8956dae0a88f1c46e39603face
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-26 01:56:06 +0900

    feat: button fade animation

---
+++ LampUIKit/LampUIKit/Core/MyTravel/View/MyTravelCategoryView.swift
@@ -0,0 +1,29 @@
+    private func setAlpha(for button: UIButton) {
+        self.myTravelButton.alpha = 0.5
+        self.myTravelButton.setTitleColor(.gray, for: .normal)
+        self.favoritePlaceButton.alpha = 0.5
+        self.favoritePlaceButton.setTitleColor(.gray, for: .normal)
+        self.completedTravelButton.alpha = 0.5
+        self.completedTravelButton.setTitleColor(.gray, for: .normal)
+        
+        button.alpha = 1.0
+        button.setTitleColor(.midNavy, for: .normal)
+    }
+    private func animateIndicator(to index: Int) {
+        
+        var button: UIButton
+        
+        switch index {
+        case 0:
+            button = myTravelButton
+        case 1:
+            button = favoritePlaceButton
+        case 2:
+            button = completedTravelButton
+        default:
+            button = myTravelButton
+        }
+        
+        setAlpha(for: button)
+        
+    }
