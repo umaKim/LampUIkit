d5b6fed1c54bf65dce1d8921fbb3956873f83aa2
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-26 01:57:22 +0900

    feat: set up constraints

--- LampUIKit/LampUIKit/Core/MyTravel/View/MyTravelCategoryView.swift
+++ LampUIKit/LampUIKit/Core/MyTravel/View/MyTravelCategoryView.swift
@@ -7,7 +7,63 @@ enum MenuBarButtonAction {
     private(set) lazy var actionPublisher = actionSubject.eraseToAnyPublisher()
     private let actionSubject = PassthroughSubject<MenuBarButtonAction, Never>()
     private var cancellable: Set<AnyCancellable>
+    
+    private lazy var myTravelButton: UIButton = {
+       let bt = UIButton()
+        bt.setTitle("나의 여행지", for: .normal)
+        bt.setTitleColor(UIColor.lightNavy, for: .normal)
+        bt.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        return bt
+    }()
+    
+    private lazy var favoritePlaceButton: UIButton = {
+       let bt = UIButton()
+        bt.setTitle("찜한 장소", for: .normal)
+        bt.setTitleColor(UIColor.lightNavy, for: .normal)
+        bt.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        return bt
+    }()
+    
+    private lazy var completedTravelButton: UIButton = {
+       let bt = UIButton()
+        bt.setTitle("완료된 여행", for: .normal)
+        bt.setTitleColor(UIColor.lightNavy, for: .normal)
+        bt.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        return bt
+    }()
+    
+    private lazy var selectionBarView: UIView = {
+        let uv = UIView(frame: .init(x: 0, y: 56, width: UIScreen.main.width / 3, height: 3))
+        uv.backgroundColor = .lightNavy
+        return uv
+    }()
+    
+    private let separatorView = DividerView()
         self.cancellable = .init()
+        let sv = UIStackView(arrangedSubviews: [myTravelButton, favoritePlaceButton, completedTravelButton])
+        sv.axis = .horizontal
+        sv.distribution = .fillEqually
+        sv.alignment = .fill
+        
+        [sv, separatorView].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(uv)
+        }
+        
+        addSubview(selectionBarView)
+    
+        NSLayoutConstraint.activate([
+            sv.leadingAnchor.constraint(equalTo: leadingAnchor),
+            sv.trailingAnchor.constraint(equalTo: trailingAnchor),
+            sv.bottomAnchor.constraint(equalTo: bottomAnchor),
+            sv.topAnchor.constraint(equalTo: topAnchor),
+            
+            selectionBarView.bottomAnchor.constraint(equalTo: separatorView.topAnchor),
+            
+            separatorView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            separatorView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            separatorView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
     private func setAlpha(for button: UIButton) {
         self.myTravelButton.alpha = 0.5
         self.myTravelButton.setTitleColor(.gray, for: .normal)
