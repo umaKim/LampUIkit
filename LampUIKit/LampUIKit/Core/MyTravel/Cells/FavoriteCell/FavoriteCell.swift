91bd5ddc7d1ed17aeb896c8e1402ebb2df826f64
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-28 20:15:00 +0900

    chore: set up class

---
+++ LampUIKit/LampUIKit/Core/MyTravel/Cells/FavoriteCell/FavoriteCell.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteCell.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/07/18.
+//
+
+import UIKit
+
+final class FavoriteCell: UICollectionViewCell {
+    static let identifier = "FavoriteCell"
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
