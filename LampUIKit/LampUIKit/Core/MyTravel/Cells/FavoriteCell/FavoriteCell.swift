a3e0841dca778dc6d1b6f83c397b948d02de7518
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-28 20:15:46 +0900

    feat: set up diffableDatasource

--- LampUIKit/LampUIKit/Core/MyTravel/Cells/FavoriteCell/FavoriteCell.swift
+++ LampUIKit/LampUIKit/Core/MyTravel/Cells/FavoriteCell/FavoriteCell.swift
@@ -9,7 +9,71 @@ import UIKit
 
 final class FavoriteCell: UICollectionViewCell {
     static let identifier = "FavoriteCell"
+    
+    private typealias DataSource = UICollectionViewDiffableDataSource<Section, MyTravelLocations>
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, MyTravelLocations>
+    
+    enum Section { case main }
+    
+    private var dataSource: DataSource?
+    
+    private lazy var collectionView: UICollectionView = {
+        let cl = UICollectionViewFlowLayout()
+        cl.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: cl)
+        cv.register(FavoriteCellHeaderCell.self,
+                    forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                    withReuseIdentifier: FavoriteCellHeaderCell.identifier)
+        cv.register(FavoriteCellCollectionViewCell.self, forCellWithReuseIdentifier: FavoriteCellCollectionViewCell.identifier)
+        cv.backgroundColor = .greyshWhite
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+    private func configureCollectionView() {
+        collectionView.delegate = self
+        
+        dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, model in
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: FavoriteCellCollectionViewCell.identifier,
+                for: indexPath) as? FavoriteCellCollectionViewCell else { return nil }
+            cell.delegate = self
+            cell.tag = indexPath.item
+            cell.configure(self.models[indexPath.item])
+//            cell.showDeleButton = self.showDeleteButton
+            return cell
+        }
+        
+        dataSource?.supplementaryViewProvider = { collectionView, kind, indexPath in
+            guard kind == UICollectionView.elementKindSectionHeader else { return nil }
+            let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: FavoriteCellHeaderCell.identifier, for: indexPath) as? FavoriteCellHeaderCell
+            view?.delegate = self
+            return view
+        }
+    }
+    
+    private func setupUI() {
+        configureCollectionView()
+        
+        backgroundColor = .systemCyan
+        
+        [collectionView].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(uv)
+        }
+        
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
 }
