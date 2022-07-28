6d41256c85db8822795ff18ab919d2f1342723b8
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-28 20:13:38 +0900

    feat: set up AR view

--- LampUIKit/LampUIKit/Core/Ar/ARViewController.swift
+++ LampUIKit/LampUIKit/Core/Ar/ARViewController.swift
@@ -21,6 +21,25 @@ class ARViewController: UIViewController {
     init(vm: ARViewModel) {
         self.viewModel = vm
         super.init(nibName: nil, bundle: nil)
+        
+        
+    }
+    
+    private lazy var dismissButton: UIButton = {
+       let bt = UIButton()
+        bt.setImage(UIImage(systemName: ""), for: .normal)
+        return bt
+    }()
+    
+    private func setupUI() {
+        [dismissButton].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(uv)
+        }
+        
+        NSLayoutConstraint.activate([
+            
+        ])
     }
     
     required init?(coder: NSCoder) {
@@ -34,6 +53,23 @@ class ARViewController: UIViewController {
         view.addSubview(sceneLocationView)
         
         sceneLocationView.locationNodeTouchDelegate = self
+        
+        //남산 타워
+//        37.551502,126.988597
+        let coordinate = CLLocationCoordinate2D(latitude: 37.551502, longitude: 126.988597)
+        let location = CLLocation(coordinate: coordinate, altitude: 300)
+        let image = UIImage(systemName: "person")!
+
+        let annotationNode = LocationAnnotationNode(location: location, image: image)
+        
+//        37.477526,127.036416
+        let coordinate2 = CLLocationCoordinate2D(latitude: 37.477526, longitude: 127.036416)
+        let location2 = CLLocation(coordinate: coordinate2, altitude: 300)
+        let image2 = UIImage(systemName: "house")!
+
+        let annotationNode2 = LocationAnnotationNode(location: location2, image: image2)
+        
+        sceneLocationView.addLocationNodesWithConfirmedLocation(locationNodes: [annotationNode, annotationNode2])
     }
     
     override func viewDidLayoutSubviews() {
