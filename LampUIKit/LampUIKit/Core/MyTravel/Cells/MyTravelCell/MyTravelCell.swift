1792975b7f3f9d8c993d564936719a9bfe4150ea
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-30 22:56:45 +0900

    feat: set up ui

---
+++ LampUIKit/LampUIKit/Core/MyTravel/Cells/MyTravelCell/MyTravelCell.swift
@@ -0,0 +1,18 @@
+        setupUI()
+    private func setupUI() {
+        configureCollectionView()
+        
+        backgroundColor = .systemCyan
+        
+        [collectionView].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(uv)
+        }
+        
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
