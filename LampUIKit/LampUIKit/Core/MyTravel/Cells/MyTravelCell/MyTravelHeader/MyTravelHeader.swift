9a496cc627f451e807b3c5f0dd8a6d0d17d606ad
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-30 23:21:53 +0900

    chore: set delegate

--- LampUIKit/LampUIKit/Core/MyTravel/Cells/MyTravelCell/MyTravelHeader/MyTravelHeader.swift
+++ LampUIKit/LampUIKit/Core/MyTravel/Cells/MyTravelCell/MyTravelHeader/MyTravelHeader.swift
@@ -1,10 +1,19 @@
+protocol MyTravelCellHeaderCellDelegate:AnyObject {
+    func myTravelCellHeaderCellDidSelectEdit()
+    func myTravelCellHeaderCellDidSelectComplete()
+}
 final class MyTravelCellHeaderCell: UICollectionReusableView {
     static let identifier = "MyTravelCellHeaderCell"
+    
+    weak var delegate: MyTravelCellHeaderCellDelegate?
+    
     private var cancellables: Set<AnyCancellable>
     
     override init(frame: CGRect) {
         self.cancellables = .init()
         super.init(frame: frame)
+                    self.delegate?.myTravelCellHeaderCellDidSelectEdit()
+                    self.delegate?.myTravelCellHeaderCellDidSelectComplete()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
