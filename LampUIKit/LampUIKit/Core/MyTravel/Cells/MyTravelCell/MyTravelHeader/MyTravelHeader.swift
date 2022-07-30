f896251eb1c9ecf95b9cb9a7e43fdb2ba6aac904
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-30 23:22:48 +0900

    feat: set up UI View

--- LampUIKit/LampUIKit/Core/MyTravel/Cells/MyTravelCell/MyTravelHeader/MyTravelHeader.swift
+++ LampUIKit/LampUIKit/Core/MyTravel/Cells/MyTravelCell/MyTravelHeader/MyTravelHeader.swift
@@ -11,11 +11,26 @@ protocol MyTravelCellHeaderCellDelegate:AnyObject {
     func myTravelCellHeaderCellDidSelectEdit()
     func myTravelCellHeaderCellDidSelectComplete()
 }
+
 final class MyTravelCellHeaderCell: UICollectionReusableView {
     static let identifier = "MyTravelCellHeaderCell"
     
     weak var delegate: MyTravelCellHeaderCellDelegate?
     
+    private lazy var goalDateLabel: UILabel = {
+       let ul = UILabel()
+        ul.text = "목표 날짜"
+        ul.textColor = .black
+        ul.font = .systemFont(ofSize: 15, weight: .semibold)
+        return ul
+    }()
+    
+    private lazy var editButton: UIButton = {
+        let bt = UIButton(type: .system)
+        bt.setTitle("편집", for: .normal)
+        bt.setTitleColor(.midNavy, for: .normal)
+        return bt
+    }()
     private var cancellables: Set<AnyCancellable>
     
     override init(frame: CGRect) {
@@ -23,7 +38,17 @@ final class MyTravelCellHeaderCell: UICollectionReusableView {
         super.init(frame: frame)
                     self.delegate?.myTravelCellHeaderCellDidSelectEdit()
                     self.delegate?.myTravelCellHeaderCellDidSelectComplete()
+        [editButton].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(uv)
+        }
+        
+        NSLayoutConstraint.activate([
+            editButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            editButton.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
     }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
