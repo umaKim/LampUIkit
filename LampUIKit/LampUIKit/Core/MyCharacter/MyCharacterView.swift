4e660556d0fa538de9ccf8323947f5caa253891f
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-30 23:39:05 +0900

    feat: set up MyCharacter view

--- LampUIKit/LampUIKit/Core/MyCharacter/MyCharacterView.swift
+++ LampUIKit/LampUIKit/Core/MyCharacter/MyCharacterView.swift
@@ -7,6 +7,39 @@
 
 import UIKit
 
-class MyCharacterView: UIView {
+class MyCharacterView: BaseWhiteView {
 
+    private(set) lazy var tableView: UITableView = {
+      let tv = UITableView()
+        tv.register(MyCharacterViewTableViewHeaderCell.self, forHeaderFooterViewReuseIdentifier: MyCharacterViewTableViewHeaderCell.identifier)
+        tv.register(MyCharacterViewTableViewCell.self, forCellReuseIdentifier: MyCharacterViewTableViewCell.identifier)
+        tv.rowHeight = 90
+        tv.backgroundColor = .greyshWhite
+        tv.sectionHeaderTopPadding = 0
+        tv.isUserInteractionEnabled = false
+        return tv
+    }()
+    
+    override init() {
+        super.init()
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: topAnchor)
+        ])
+    }
 }
+
