be219aa2248107ecebf45cdc88ee13bcec7efd08
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-30 23:40:34 +0900

    chore: declare MyCharacterVC

--- LampUIKit/LampUIKit/Core/MyCharacter/MyCharacterViewController.swift
+++ LampUIKit/LampUIKit/Core/MyCharacter/MyCharacterViewController.swift
@@ -7,14 +7,27 @@
 
 import UIKit
 
-class MyCharacterViewController: UIViewController {
+class MyCharacterViewController: BaseViewContronller {
 
+    private let viewModel: MyCharacterViewModel
+    
+    init(vm: MyCharacterViewModel) {
+        self.viewModel = vm
+        super.init()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+}
 
     /*
     // MARK: - Navigation
