0b5d3b047d521525873c9444555e32b59c2b2b0a
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-01 04:03:26 +0900

    chore: set up bind method

--- LampUIKit/LampUIKit/Core/InitialSetPage/Quiz/InitialQuizViewController.swift
+++ LampUIKit/LampUIKit/Core/InitialSetPage/Quiz/InitialQuizViewController.swift
@@ -10,4 +10,38 @@ import UIKit
 import Combine
 
 class InitialQuizViewController: BaseViewContronller {
+        bind()
+    private func bind() {
+        contentView
+            .actionPublisher
+            .sink { action in
+                switch action {
+                case .button1:
+                    break
+                    
+                case .button2:
+                    break
+                    
+                case .button3:
+                    break
+                }
+            }
+            .store(in: &cancellables)
+        
+        viewModel
+            .notifyPublisher
+            .sink { noti in
+                switch noti {
+                case .question(let text):
+                    print(text)
+                case .index(let index):
+                    print(index)
+                case .answers(let answer1, let answer2, let answer3):
+                    print(answer1)
+                    print(answer2)
+                    print(answer3)
+                }
+            }
+            .store(in: &cancellables)
+    }
 }
