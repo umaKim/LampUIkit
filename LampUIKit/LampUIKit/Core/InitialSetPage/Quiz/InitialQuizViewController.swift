813fb79ac76e0afea9a7ff12d778ef94fafecda5
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-01 04:02:56 +0900

    chore: declare class

---
+++ LampUIKit/LampUIKit/Core/InitialSetPage/Quiz/InitialQuizViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InitialQuizViewController.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/07/30.
+//
+
+import UIKit
+
+import Combine
+
+class InitialQuizViewController: BaseViewContronller {
+}
