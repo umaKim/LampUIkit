09d8461c88ee43260b9f027b0b7246bd367f2b24
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-01 03:58:18 +0900

    feat: set up ui and its constraints

--- LampUIKit/LampUIKit/Core/MyCharacter/Views/CapsuleLabelView.swift
+++ LampUIKit/LampUIKit/Core/MyCharacter/Views/CapsuleLabelView.swift
@@ -8,9 +8,32 @@
 import UIKit
 
 final class CapsuleLabelView: UIView {
+    private let label: UILabel = {
+       let lb = UILabel()
+        lb.textAlignment = .center
+        return lb
+    }()
     init() {
         super.init(frame: .zero)
         
+        setupUI()
+    }
+    private func setupUI() {
+        clipsToBounds = true
+        
+        [label].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(label)
+        }
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+            label.topAnchor.constraint(equalTo: topAnchor, constant: 3),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -3),
+            
+            heightAnchor.constraint(equalToConstant: 20),
+        ])
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
