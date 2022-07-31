93c718e9489edce0dc071b9900fa2b1589c56bab
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-01 03:57:37 +0900

    feat: declare Class

---
+++ LampUIKit/LampUIKit/Core/MyCharacter/Views/CapsuleLabelView.swift
@@ -0,0 +1,18 @@
+//
+//  CapsuleLabelView.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/07/29.
+//
+
+import UIKit
+
+final class CapsuleLabelView: UIView {
+    init() {
+        super.init(frame: .zero)
+        
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
