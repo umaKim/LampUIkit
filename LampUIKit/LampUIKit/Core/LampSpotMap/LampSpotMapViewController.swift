fc3ecfb887e94ae56e9446fed5bb341cbdb378bb
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-07-31 23:16:34 +0900

    chore: di for viewModel

--- LampUIKit/LampUIKit/Core/LampSpotMap/LampSpotMapViewController.swift
+++ LampUIKit/LampUIKit/Core/LampSpotMap/LampSpotMapViewController.swift
@@ -11,9 +11,20 @@ class LampSpotMapViewController: BaseViewContronller {
     
     private let contentView = LampSpotView()
     
+    private let viewModel: LampSpotMapViewModel
+    
     override func loadView() {
         super.loadView()
         
         view = contentView
+    }
+    
+    init(vm: LampSpotMapViewModel) {
+        self.viewModel = vm
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
