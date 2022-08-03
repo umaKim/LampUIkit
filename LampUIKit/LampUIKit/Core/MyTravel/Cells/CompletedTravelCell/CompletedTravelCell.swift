fe9c117ef03dde0806006424ff3821cf3d6f3dea
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-03 21:21:39 +0900

    feat: set up ui components

--- LampUIKit/LampUIKit/Core/MyTravel/Cells/CompletedTravelCell/CompletedTravelCell.swift
+++ LampUIKit/LampUIKit/Core/MyTravel/Cells/CompletedTravelCell/CompletedTravelCell.swift
@@ -22,6 +22,46 @@ final class CompletedTravelHeaderCell: UICollectionReusableView {
 
 final class CompletedTravelCellCollectionViewCell: UICollectionViewCell {
     static let identifier = "CompletedTravelCellCollectionViewCell"
+    
+    private lazy var backgroundImageView: UIImageView = {
+       let uv = UIImageView()
+        uv.image = UIImage(named: "ticketBackground")
+        return uv
+    }()
+    
+    private lazy var visitiedDateLabel: UILabel = {
+       let lb = UILabel()
+        lb.font = .systemFont(ofSize: 18, weight: .semibold)
+        lb.textColor = .black
+        return lb
+    }()
+    
+    private lazy var locationNameLabel: UILabel = {
+       let lb = UILabel()
+        lb.font = .systemFont(ofSize: 18, weight: .semibold)
+        lb.textColor = .midNavy
+        return lb
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundView = backgroundImageView
+        configureShadow()
+        
+        [visitiedDateLabel, locationNameLabel].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(uv)
+        }
+        
+        NSLayoutConstraint.activate([
+            visitiedDateLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 46),
+            visitiedDateLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 36),
+            
+            locationNameLabel.leadingAnchor.constraint(equalTo: visitiedDateLabel.leadingAnchor),
+            locationNameLabel.topAnchor.constraint(equalTo: visitiedDateLabel.bottomAnchor, constant: 20),
+        ])
+    }
     private lazy var collectionView: UICollectionView = {
         let cl = UICollectionViewFlowLayout()
         cl.scrollDirection = .vertical
