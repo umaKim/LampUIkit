297efa57f27910f1f1282f5927a0e6b891eb04a6
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-03 21:21:24 +0900

    feat: set up ui

--- LampUIKit/LampUIKit/Core/MyTravel/Cells/CompletedTravelCell/CompletedTravelCell.swift
+++ LampUIKit/LampUIKit/Core/MyTravel/Cells/CompletedTravelCell/CompletedTravelCell.swift
@@ -22,10 +22,39 @@ final class CompletedTravelHeaderCell: UICollectionReusableView {
 
 final class CompletedTravelCellCollectionViewCell: UICollectionViewCell {
     static let identifier = "CompletedTravelCellCollectionViewCell"
+    private lazy var collectionView: UICollectionView = {
+        let cl = UICollectionViewFlowLayout()
+        cl.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: cl)
+        cv.register(CompletedTravelHeaderCell.self,
+                    forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                    withReuseIdentifier: CompletedTravelHeaderCell.identifier)
+        cv.register(CompletedTravelCellCollectionViewCell.self, forCellWithReuseIdentifier: CompletedTravelCellCollectionViewCell.identifier)
+        cv.backgroundColor = .greyshWhite
+        return cv
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    private func setupUI() {
+        configureCollectionView()
+        
+        backgroundColor = .systemCyan
+        
+        [collectionView].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(uv)
+        }
+        
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
 }
