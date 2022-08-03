6ac884a552fe70c23e16814c1b74e647d967bae3
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-03 21:20:18 +0900

    chore: declare CompletedTravelCell class

---
+++ LampUIKit/LampUIKit/Core/MyTravel/Cells/CompletedTravelCell/CompletedTravelCell.swift
@@ -0,0 +1,21 @@
+//
+//  CompletedTravelCell.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/07/18.
+//
+
+import UIKit
+
+final class CompletedTravelHeaderCell: UICollectionReusableView {
+    static let identifier = "CompletedTravelHeaderCell"
+   
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
