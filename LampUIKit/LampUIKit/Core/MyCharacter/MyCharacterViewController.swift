cade723d1172429bc6668a2a7e14bfeb80e5d208
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-03 21:24:53 +0900

    chore: declare class

--- LampUIKit/LampUIKit/Core/MyCharacter/MyCharacterViewController.swift
+++ LampUIKit/LampUIKit/Core/MyCharacter/MyCharacterViewController.swift
@@ -81,10 +81,7 @@ extension MyCharacterViewController: UITableViewDelegate {
     }
 }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+class GraphHeaderView: UIView {
         setupUI()
     private func setupUI() {
         let headerSv = UIStackView(arrangedSubviews: [titleLabel, numberLabel])
@@ -109,4 +106,8 @@ extension MyCharacterViewController: UITableViewDelegate {
     }
     */
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 }
