79ff77bea6ec91627189f3587717939ed970d81e
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-04 20:54:26 +0900

    feat: set up UI

--- LampUIKit/LampUIKit/Core/MyTravel/Cells/FavoriteCell/FavoriteCollectionViewCell/FavoriteCellCollectionViewCell.swift
+++ LampUIKit/LampUIKit/Core/MyTravel/Cells/FavoriteCell/FavoriteCollectionViewCell/FavoriteCellCollectionViewCell.swift
@@ -8,7 +8,69 @@ final class FavoriteCellCollectionViewCell: UICollectionViewCell {
     
     weak var delegate: FavoriteCellCollectionViewCellDelegate?
     
+    private let containerView: UIView = {
+        let uv = UIView()
+        uv.layer.cornerRadius = 6
+        uv.layer.borderColor = UIColor.systemGray.cgColor
+        uv.backgroundColor = .greyshWhite
+        return uv
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+       let lb = UILabel()
+        lb.text = "경복궁"
+        lb.textColor = .midNavy
+        lb.font = .systemFont(ofSize: 20, weight: .bold)
+        lb.numberOfLines = 1
+        return lb
+    }()
+    
+    private lazy var favoriteButton: UIButton = {
+       let bt = UIButton()
+        bt.setImage(UIImage(named: "favorite_saved"), for: .normal)
+        return bt
+    }()
+    
+    private lazy var timeLabel: UILabel = {
+       let lb = UILabel()
+        lb.font = .systemFont(ofSize: 14, weight: .semibold)
+        lb.text = "09:00~18:30 (입장마감은 17:30)"
+        lb.textColor = .midNavy
+        return lb
+    }()
+    
+    private lazy var addressLabel: UILabel = {
+       let lb = UILabel()
+        lb.text = "주소 어쩌구 저쩌구"
+        lb.textColor = .midNavy
+        lb.font = .systemFont(ofSize: 14, weight: .semibold)
+        return lb
+    }()
     override init(frame: CGRect) {
+        let totalSv = UIStackView(arrangedSubviews: [titleLabel, timeLabel, addressLabel])
+        totalSv.axis = .vertical
+        totalSv.alignment = .fill
+        totalSv.distribution = .fill
+        totalSv.spacing = 6
+        
+        [containerView, totalSv, favoriteButton].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(uv)
+        }
+        
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 6),
+            containerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -6),
+            containerView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -6),
+            containerView.topAnchor.constraint(equalTo: topAnchor, constant: 6),
+            
+            totalSv.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            totalSv.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
+            totalSv.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+            
+            favoriteButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+            favoriteButton.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 16),
+        ])
     }
     
     override func layoutSubviews() {
