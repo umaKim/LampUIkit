442c85ab64b32972943715df79fafda2e035e51b
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-05 19:22:10 +0900

    chore: declare completed travelcell collectionview cell class

--- LampUIKit/LampUIKit/Core/MyTravel/Cells/CompletedTravelCell/CompletedTravelCellCollectionViewCell/CompletedTravelCellCollectionViewCell.swift
+++ LampUIKit/LampUIKit/Core/MyTravel/Cells/CompletedTravelCell/CompletedTravelCellCollectionViewCell/CompletedTravelCellCollectionViewCell.swift
@@ -1,3 +1,15 @@
+//
+//  CompletedTravelCellCollectionViewCell.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/08/05.
+//
+
+import UIKit
+
+final class CompletedTravelCellCollectionViewCell: UICollectionViewCell {
+    static let identifier = "CompletedTravelCellCollectionViewCell"
+    
     private lazy var backgroundImageView: UIImageView = {
        let uv = UIImageView()
         uv.image = UIImage(named: "ticketBackground")
@@ -40,3 +52,7 @@
             locationNameLabel.leadingAnchor.constraint(equalTo: visitiedDateLabel.leadingAnchor),
             locationNameLabel.topAnchor.constraint(equalTo: visitiedDateLabel.bottomAnchor, constant: 20),
         ])
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
