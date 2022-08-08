9d6219d1a8a22d588fe27ef70d854bdc2b19fb6d
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-09 00:58:08 +0900

    chore: set up data stream

--- LampUIKit/LampUIKit/Core/Map/MapView.swift
+++ LampUIKit/LampUIKit/Core/Map/MapView.swift
@@ -4,22 +4,39 @@
 //
 //  Created by 김윤석 on 2022/07/22.
 //
-
+import CombineCocoa
+import Combine
 import UIKit
 
-class MapView: UIView {
+enum MapViewAction {
+    case back
+}
+
 class MapView: BaseView {
+    
+    private(set) lazy var actionPublisher = actionSubject.eraseToAnyPublisher()
+    private let actionSubject = PassthroughSubject<MapViewAction, Never>()
+    
+    private(set) lazy var backButton = UIBarButtonItem(image: .back, style: .done, target: nil, action: nil)
 
     private(set) var mapView = MTMapView()
     
     override init() {
         super.init()
+        
+        bind()
         setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func bind() {
+        backButton.tapPublisher.sink {[unowned self] _ in
+            self.actionSubject.send(.back)
+        }
+        .store(in: &cancellables)
     }
     
     private func setupUI() {
