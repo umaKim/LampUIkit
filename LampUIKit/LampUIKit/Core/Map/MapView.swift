385518181789a1ff3bc900cf4b915a1ea5d4cbbc
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-09 00:57:51 +0900

    chore: set up MapView

--- LampUIKit/LampUIKit/Core/Map/MapView.swift
+++ LampUIKit/LampUIKit/Core/Map/MapView.swift
@@ -10,20 +10,32 @@ import UIKit
 class MapView: UIView {
 class MapView: BaseView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private(set) var mapView = MTMapView()
+    
     override init() {
         super.init()
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     }
-    */
+    
+    private func setupUI() {
+        [mapView].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(uv)
+        }
+        
+        NSLayoutConstraint.activate([
+            mapView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mapView.topAnchor.constraint(equalTo: topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
 }
 
 }
