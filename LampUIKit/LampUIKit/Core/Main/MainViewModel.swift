d1e75364e834e895caa6066555b3cfdc9b3b5150
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-11 17:12:26 +0900

    chore: set fetchItems method

--- LampUIKit/LampUIKit/Core/Main/MainViewModel.swift
+++ LampUIKit/LampUIKit/Core/Main/MainViewModel.swift
@@ -17,12 +17,30 @@ class MainViewModel: BaseViewModel  {
     
     private(set) lazy var notifyPublisher = notifySubject.eraseToAnyPublisher()
     private let notifySubject = PassthroughSubject<MainViewModelNotification, Never>()
+    
+    private let network = NetworkService.shared
+    
     private let uid: String
     init(_ uid: String) {
         self.uid = uid
         super.init()
         
     }
+    
+    public func fetchItems() {
+        let location = Location(lat: longitude, long: latitude)
+        network.fetchRecommendation(location, zoomLevelDistance.getLevel().1) {[unowned self] result in
+            switch result {
+            case .success(let items):
+                self.recommendedPlaces = items.result
+                self.notifySubject.send(.recommendedLocations(items.result))
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
 }
     }
 }
