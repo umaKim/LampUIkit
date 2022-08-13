abf4e8181e9cc83b3e9dccd66b7abccafe70d018
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-13 22:46:59 +0900

    -

--- LampUIKit/LampUIKit/Core/MyPage/MyPageTableViewHeaderView.swift
+++ LampUIKit/LampUIKit/Core/MyPage/MyPageTableViewHeaderView.swift
@@ -1,3 +1,11 @@
+//
+//  MyPageTableViewHeaderView.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/08/12.
+//
+
+import UIKit
 
 class MyPageTableViewHeaderView: UIView {
     init(_ name: String, email: String) {
