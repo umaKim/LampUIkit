ba38136e0566c7543dcbc0bc36d2de3b391b92c6
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-14 21:11:13 +0900

    chore: set up loading view

--- LampUIKit/LampUIKit/Helper/UIViewController+Ext.swift
+++ LampUIKit/LampUIKit/Helper/UIViewController+Ext.swift
@@ -52,3 +52,37 @@ extension UIViewController {
     }
 }
 
+fileprivate var containerView: UIView!
+
+extension UIViewController {
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25, animations: {containerView.alpha = 0.8})
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+//        activityIndicator.tintColor = .red
+        
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
+        }
+    }
+}
