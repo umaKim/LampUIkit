70b53b95798c3195b86478bce39b7b4cb0e31506
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-14 21:10:14 +0900

    chore: set up class for RectangleTextField

---
+++ LampUIKit/LampUIKit/View/LampRectangleTextField.swift
@@ -0,0 +1,38 @@
+//
+//  LampRectangleTextField.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/08/14.
+//
+
+import UIKit
+
+class LampRectangleTextField: UITextField {
+
+    private let padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 5)
+
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        
+        self.placeholder = placeholder
+        backgroundColor = .white
+        layer.cornerRadius = 5
+        textColor = .darkNavy
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
