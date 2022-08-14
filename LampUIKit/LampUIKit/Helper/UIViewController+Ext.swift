739bc99e9d1ff95b7393182e9ab325aeb1caab46
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-14 21:11:01 +0900

    chore: method for hiding keyboard

--- LampUIKit/LampUIKit/Helper/UIViewController+Ext.swift
+++ LampUIKit/LampUIKit/Helper/UIViewController+Ext.swift
@@ -39,3 +39,16 @@ extension UIViewController {
         (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.isInitialSettingDone = bool
     }
 }
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
