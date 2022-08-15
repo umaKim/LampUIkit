fe0c27f397a541dd8f03cc8c37b53675c13d0881
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-15 23:01:30 +0900

    chore: message for allowing access to photo library

--- LampUIKit/LampUIKit/Core/InitialSetPage/Quiz/InitialQuizView.swift
+++ LampUIKit/LampUIKit/Core/InitialSetPage/Quiz/InitialQuizView.swift
@@ -1,3 +1,14 @@
+//
+//  InitialQuizView.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/07/30.
+//
+
+import CombineCocoa
+import Combine
+import UIKit
+
 enum InitialQuizViewAction {
     case button1
     case button2
@@ -6,9 +17,47 @@ enum InitialQuizViewAction {
     case button5
     case next
 }
+
+class AnswerButton: UIButton {
+    
+    init() {
+        super.init(frame: .zero)
+        
+        backgroundColor = .darkNavy
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.midNavy.cgColor
+        widthAnchor.constraint(equalToConstant: 280).isActive = true
+        heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        titleLabel?.numberOfLines = 0; // Dynamic number of lines
+        titleLabel?.lineBreakMode = .byWordWrapping;
+        titleLabel?.textAlignment = .center
+        
+        titleEdgeInsets = UIEdgeInsets(top: 10,left: 10,bottom: 10,right: 10)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class InitialQuizView: BaseView {
+    
     private(set) lazy var actionPublisher = actionSubject.eraseToAnyPublisher()
     private let actionSubject = PassthroughSubject<InitialQuizViewAction, Never>()
+    
+    private let quizView = QuizView()
+    private let resultView = QuizResultView()
+    
+    override init() {
+        super.init()
+        
+        backgroundColor = .darkNavy
+        
         bind()
+        setupUI()
+    }
+    
     private func bind() {
         quizView
             .actionPublisher
@@ -45,6 +94,32 @@ enum InitialQuizViewAction {
             }
             .store(in: &cancellables)
     }
+    
+    private func setupUI() {
+        [quizView, resultView].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(uv)
+        }
+        
+        NSLayoutConstraint.activate([
+            quizView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            quizView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            quizView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            quizView.topAnchor.constraint(equalTo: topAnchor),
+            
+            resultView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            resultView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            resultView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            resultView.topAnchor.constraint(equalTo: topAnchor),
+        ])
+        
+        resultView.isHidden = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     public func setQuizData(_ data: Question) {
         quizView.setQuizData(data)
     }
@@ -55,3 +130,4 @@ enum InitialQuizViewAction {
         
         resultView.setupCharacter(data)
     }
+}
