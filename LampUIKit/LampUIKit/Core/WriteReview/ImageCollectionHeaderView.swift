edb89738125d62274ffd1611c094ef315ff5e255
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-15 23:22:49 +0900

    chore: declare class

---
+++ LampUIKit/LampUIKit/Core/WriteReview/ImageCollectionHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionHeaderView.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/08/15.
+//
+import CombineCocoa
+import Combine
+import UIKit
+
+class ImageCollectionHeaderView: UICollectionReusableView {
+    override init(frame: CGRect) {
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
