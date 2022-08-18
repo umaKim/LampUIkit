9455181c3b2754499938060058ec8a21e95f3bb8
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-18 10:28:05 +0900

    chore: set character models

--- LampUIKit/LampUIKit/Core/InitialSetPage/Quiz/InitialQuizViewModel.swift
+++ LampUIKit/LampUIKit/Core/InitialSetPage/Quiz/InitialQuizViewModel.swift
@@ -17,6 +17,50 @@ class InitialQuizViewModel: BaseViewModel {
     private(set) lazy var notifyPublisher = notifySubject.eraseToAnyPublisher()
     private let notifySubject = PassthroughSubject<InitialQuizViewModelNotify, Never>()
     
+    private let characterImages: [UIImage?] = [
+        .init(named: "bear"),
+        .init(named: "cat"),
+        .init(named: "racoon"),
+        .init(named: "dog"),
+        .init(named: "rabbit"),
+    ]
+    
+    
+    private lazy var characters: [CharacterData] = [.init(characterName: "Bear",
+                                                     level: "1",
+                                                     image: characterImages[0],
+                                                     averageStat: "",
+                                                     mileage: "",
+                                                     gaugeDatum: []),
+                                               
+                                               .init(characterName: "Cat",
+                                                     level: "1",
+                                                     image: characterImages[1],
+                                                     averageStat: "",
+                                                     mileage: "",
+                                                     gaugeDatum: []),
+                                               
+                                                .init(characterName: "Racoon",
+                                                      level: "1",
+                                                      image: characterImages[2],
+                                                      averageStat: "",
+                                                      mileage: "",
+                                                      gaugeDatum: []),
+                                               
+                                               .init(characterName: "Dog",
+                                                     level: "1",
+                                                     image: characterImages[3],
+                                                     averageStat: "",
+                                                     mileage: "",
+                                                     gaugeDatum: []),
+                                               
+                                               .init(characterName: "Rabbit",
+                                                     level: "1",
+                                                     image: characterImages[4],
+                                                     averageStat: "",
+                                                     mileage: "",
+                                                     gaugeDatum: [])]
+    
     override init() {
         super.init()
         
