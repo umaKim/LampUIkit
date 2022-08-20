1bf5ec719fcce895b27b5a0e9b5bb683d39be861
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-20 21:49:48 +0900

    chore: declare dataStream

--- LampUIKit/LampUIKit/Core/MyTravel/MyTravelViewModel.swift
+++ LampUIKit/LampUIKit/Core/MyTravel/MyTravelViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by 김윤석 on 2022/07/16.
 //
 
+import Combine
 import Foundation
 
 enum MyTravelViewModelNotify {
@@ -12,6 +13,9 @@ enum MyTravelViewModelNotify {
 }
 
 class MyTravelViewModel: BaseViewModel {
+    private(set) lazy var notifyPublisher = notifySubject.eraseToAnyPublisher()
+    private let notifySubject = PassthroughSubject<MyTravelViewModelNotify, Never>()
+    
     private(set) var model: MyTravelDataSet
     
     override init() {
