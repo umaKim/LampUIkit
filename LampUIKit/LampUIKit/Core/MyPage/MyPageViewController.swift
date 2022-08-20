818e6a8a74281edd92a2c8d3a6b71ad12b1f9be6
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-20 22:33:03 +0900

    -

--- LampUIKit/LampUIKit/Core/MyPage/MyPageViewController.swift
+++ LampUIKit/LampUIKit/Core/MyPage/MyPageViewController.swift
@@ -119,14 +119,3 @@ extension MyPageViewController: UITableViewDelegate {
         100
     }
 }
-
-class MyPageTableViewHeaderView: UIView {
-    init() {
-        super.init(frame: .zero)
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
