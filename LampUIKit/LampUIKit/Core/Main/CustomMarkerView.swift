1783c29fa6a7360f1dbac6a17c7fdc518918af0f
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-22 20:11:27 +0900

    chore: add images

---
+++ LampUIKit/LampUIKit/Core/Main/CustomMarkerView.swift
@@ -0,0 +1,49 @@
+//
+//  CustomMarkerView.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/08/22.
+//
+
+import UIKit
+
+class CustomMarkerView: UIView {
+    
+    private let outerImageView: UIImageView = {
+       let uv = UIImageView()
+        uv.image = UIImage(named: "circleRecommended")
+        return uv
+    }()
+    
+    private let contentImageView: UIImageView = {
+       let uv = UIImageView()
+        uv.contentMode = .scaleToFill
+        return uv
+    }()
+    
+    private let width = 60
+    private let height = 60
+    
+    init() {
+        super.init(frame: .init(x: 0, y: 0, width: width, height: height))
+    }
+    
+    convenience init(of imageUrl: String) {
+        self.init()
+        guard let url = URL(string: imageUrl) else { return }
+        contentImageView.sd_setImage(with: url, placeholderImage: UIImage(named: "placeholder"))
+        contentImageView.layer.cornerRadius = CGFloat(width/2)
+        contentImageView.clipsToBounds = true
+        addSubview(contentImageView)
+        contentImageView.frame = .init(x: 0, y: 0, width: width, height: height - 6)
+        
+        addSubview(outerImageView)
+        outerImageView.frame = .init(x: 0, y: 0, width: width, height: height)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
