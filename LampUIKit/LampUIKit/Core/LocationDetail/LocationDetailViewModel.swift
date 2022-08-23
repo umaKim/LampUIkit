3eab12dfe61e7f7420868f60ce9e41d7746b1809
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-23 21:04:46 +0900

    chore: set up location detailImage networking

--- LampUIKit/LampUIKit/Core/LocationDetail/LocationDetailViewModel.swift
+++ LampUIKit/LampUIKit/Core/LocationDetail/LocationDetailViewModel.swift
@@ -52,7 +52,23 @@ final class LocationDetailViewModel: BaseViewModel {
             
             self.notifySubject.send(.endLoading)
         }
+        
+        NetworkService.shared.fetchLocationDetailImage(contentId) { result in
+            switch result {
+            case .success(let response):
+                var images = [String]()
+                
+                if let mainImage = self.location?.image {
+                    images.append(mainImage)
+                }
+
+                images.append(contentsOf: response.image)
                 self.notifySubject.send(.locationDetailImages(images))
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
     private func postAddToMyTrip() {
