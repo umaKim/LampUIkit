a954521e9a786c9bbebf4e2f3ea0bf5055a22894
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-24 10:18:19 +0900

    chore: delete unneecessary lines

--- LampUIKit/LampUIKit/Core/Main/MainView.swift
+++ LampUIKit/LampUIKit/Core/Main/MainView.swift
@@ -190,23 +190,3 @@ class MainView: BaseWhiteView {
         ])
     }
 }
-
-struct LocationData: Decodable {
-    let documents: [KLDocument]
-}
-
-struct KLDocument: Decodable {
-    let placeName: String
-    let addressName: String
-    let roadAddressName: String
-    let x: String
-    let y: String
-    let distance: String
-    
-    enum CodingKeys: String, CodingKey {
-        case x, y, distance
-        case placeName = "place_name"
-        case addressName = "address_name"
-        case roadAddressName = "road_address_name"
-    }
-}
