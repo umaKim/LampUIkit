2c8a9fc8df968b23088d880ea27d8ff6078b3715
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-25 12:01:04 +0900

    chore: add UIkit

--- LampUIKit/LampUIKit/HapticManager.swift
+++ LampUIKit/LampUIKit/HapticManager.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 김윤석 on 2022/08/24.
 //
-
+import UIKit
 import Foundation
 
 class HapticManager {
--- LampUIKit/LampUIKit/Helper/UIImage+Ext.swift
+++ LampUIKit/LampUIKit/Helper/UIImage+Ext.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 김윤석 on 2022/08/12.
 //
-
+import UIKit
 import Foundation
 
 extension UIImage {
