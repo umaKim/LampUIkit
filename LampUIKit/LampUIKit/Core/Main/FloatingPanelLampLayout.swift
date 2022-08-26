7e45b1a31b566b36f45268eed7b3edc093b9b543
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-26 21:16:51 +0900

    chore: declare FloatingPanelLampLayout class

---
+++ LampUIKit/LampUIKit/Core/Main/FloatingPanelLampLayout.swift
@@ -0,0 +1,25 @@
+//
+//  FloatingPanelLampLayout.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/08/25.
+//
+import FloatingPanel
+import UIKit
+
+class FloatingPanelLampLayout: FloatingPanelLayout {
+    let position: FloatingPanelPosition = .bottom
+    let initialState: FloatingPanelState = .tip
+
+    var anchors: [FloatingPanelState: FloatingPanelLayoutAnchoring] {
+        return [
+            .full: FloatingPanelLayoutAnchor(absoluteInset: 12, edge: .top, referenceGuide: .safeArea),
+            .half: FloatingPanelLayoutAnchor(absoluteInset: 302.0, edge: .bottom, referenceGuide: .safeArea),
+            .tip: FloatingPanelLayoutAnchor(absoluteInset: 85.0, edge: .bottom, referenceGuide: .safeArea),
+        ]
+    }
+
+    func backdropAlpha(for state: FloatingPanelState) -> CGFloat {
+        return 0.0
+    }
+}
