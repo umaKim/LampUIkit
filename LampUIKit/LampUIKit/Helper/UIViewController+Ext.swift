a0a6869c398f21ef11040c83f0cb1638d1def5b8
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-26 21:13:26 +0900

    chore: add UIViewcontroller extension

--- LampUIKit/LampUIKit/Helper/UIViewController+Ext.swift
+++ LampUIKit/LampUIKit/Helper/UIViewController+Ext.swift
@@ -95,3 +95,19 @@ extension UIViewController {
         }
     }
 }
+
+extension UIViewController {
+    var isModal: Bool {
+        if let index = navigationController?.viewControllers.firstIndex(of: self), index > 0 {
+            return false
+        } else if presentingViewController != nil {
+            return true
+        } else if navigationController?.presentingViewController?.presentedViewController == navigationController {
+            return true
+        } else if tabBarController?.presentingViewController is UITabBarController {
+            return true
+        } else {
+            return false
+        }
+    }
+}
