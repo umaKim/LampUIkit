869eb3347e899f4e31ca55eef19419fc53794005
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-27 19:40:48 +0900

    chore: declare custom InputTextView

---
+++ LampUIKit/LampUIKit/View/InputTextView.swift
@@ -0,0 +1,47 @@
+//
+//  InputTextView.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/08/18.
+//
+
+import UIKit
+
+class InputTextView: UITextView {
+    var placeholderText: String? {
+        didSet {
+            placeholderLabel.text  = placeholderText
+        }
+    }
+    
+    private let placeholderLabel: UILabel = {
+       let lb = UILabel()
+        lb.textColor = .lightGray
+        return lb
+    }()
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        
+        textColor = .black
+        
+        placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(placeholderLabel)
+        
+        NSLayoutConstraint.activate([
+            placeholderLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            placeholderLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 8),
+        ])
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidChange), name: UITextView.textDidChangeNotification, object: nil)
+    }
+    
+    @objc
+    private func textFieldDidChange() {
+        placeholderLabel.isHidden = !text.isEmpty
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
