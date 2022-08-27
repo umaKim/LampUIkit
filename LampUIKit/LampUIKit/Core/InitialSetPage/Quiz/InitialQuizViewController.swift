919077d455ebbec3077e02a460323b2a992de8b7
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-28 06:25:38 +0900

    chore: declare required init

--- LampUIKit/LampUIKit/Core/InitialSetPage/Quiz/InitialQuizViewController.swift
+++ LampUIKit/LampUIKit/Core/InitialSetPage/Quiz/InitialQuizViewController.swift
@@ -23,6 +23,11 @@ class InitialQuizViewController: BaseViewContronller {
         
         bind()
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func loadView() {
         super.loadView()
         
