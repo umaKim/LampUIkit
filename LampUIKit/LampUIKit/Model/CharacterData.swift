ecf57f3a4a7a3af716e448e69b2343f9a05e700f
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-08-29 01:27:53 +0900

    chore: change variable name and data type

--- LampUIKit/LampUIKit/Model/CharacterData.swift
+++ LampUIKit/LampUIKit/Model/CharacterData.swift
@@ -4,13 +4,13 @@
 //
 //  Created by 김윤석 on 2022/08/14.
 //
-
+import UIKit
 import Foundation
 
 struct CharacterData {
     let characterName: String
     let level: String
-    let image: String
+    let imageString: String?
     let averageStat: String
     let mileage: String
     let gaugeDatum: [GaugeData]
