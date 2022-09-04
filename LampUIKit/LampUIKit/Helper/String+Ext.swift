3b7bb92e8bb58aebf2ed6adb4c98d9c2ce799314
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-09-04 22:56:54 +0900

    chore: add string extension to enliminate html tags

---
+++ LampUIKit/LampUIKit/Helper/String+Ext.swift
@@ -0,0 +1,33 @@
+//
+//  String+Ext.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/09/04.
+//
+
+import Foundation
+
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return nil }
+        do {
+            let font = UIFont.systemFont(ofSize: 72)
+            let attributes = [NSAttributedString.Key.font: font]
+            let string = try NSAttributedString(data: data,
+                                                options: [
+                                                  .documentType:
+                                                      NSAttributedString.DocumentType.html,
+                                                  .characterEncoding:
+                                                      String.Encoding.utf8.rawValue
+                                                ],
+                                                documentAttributes: nil)
+            return string
+        } catch {
+            return nil
+        }
+    }
+    
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
