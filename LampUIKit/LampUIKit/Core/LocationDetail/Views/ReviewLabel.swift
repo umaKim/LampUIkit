5da3226c07195f5e109ae08ae015779ae04e492b
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-09-06 23:41:09 +0900

    chore: declare ReviewLabel

---
+++ LampUIKit/LampUIKit/Core/LocationDetail/Views/ReviewLabel.swift
@@ -0,0 +1,58 @@
+//
+//  ReviewLabel.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/09/05.
+//
+
+import UIKit
+
+class ReviewLabel: UIView {
+    private let titleLabel: UILabel = {
+        let lb = UILabel()
+        lb.font = .systemFont(ofSize: 15, weight: .semibold)
+        lb.textColor = .midNavy
+        lb.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        return lb
+    }()
+    
+    private lazy var subTitleLabel = RoundedLabelView("")
+    
+    public func setSubtitle(_ text: String) {
+        self.subTitleLabel.setText(text)
+    }
+    
+    init(
+        title: String,
+        subTitle: String,
+        spacing: CGFloat = 16,
+        setRoundDesign: Bool = true
+    ) {
+        self.titleLabel.text = title
+        super.init(frame: .zero)
+        self.subTitleLabel.setText(subTitle)
+        self.subTitleLabel.setRound(setRoundDesign)
+        
+        let sv = UIStackView(arrangedSubviews: [titleLabel, subTitleLabel])
+        sv.axis = .horizontal
+        sv.alignment = .fill
+        sv.distribution = .fill
+        sv.spacing = spacing
+        
+        [sv].forEach { uv in
+            uv.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(sv)
+        }
+        
+        NSLayoutConstraint.activate([
+            sv.leadingAnchor.constraint(equalTo: leadingAnchor),
+            sv.topAnchor.constraint(equalTo: topAnchor),
+            sv.bottomAnchor.constraint(equalTo: bottomAnchor),
+            sv.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
