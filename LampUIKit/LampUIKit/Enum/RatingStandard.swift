b07513e2b1043ff62e6aefbea3cb0fdaccc4b66f
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-09-11 03:27:15 +0900

    fix: take away unnecessary rating

--- LampUIKit/LampUIKit/Enum/RatingStandard.swift
+++ LampUIKit/LampUIKit/Enum/RatingStandard.swift
@@ -10,6 +10,6 @@ import Foundation
 enum RatingStandard {
     static let comfort: [String] = ["-", "매우 만족", "만족", "보통"]
     static let atmosphere: [String] = ["-", "고즈넉한", "잔잔한", "생기넘치는", "푸르른"]
-    static let surrounding: [String] = ["-", "여유로운", "혼잡한", "인파가 적당한"]
+    static let surrounding: [String] = ["-", "여유로운", "혼잡한"]
     static let food: [String] = ["-", "다양한 종류", "먹거리 없음"]
 }
