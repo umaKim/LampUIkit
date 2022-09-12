de200d21f8d105736e9ab7cc65b6c12dfbfbcc29
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-09-12 14:17:57 +0900

    chore: change data type

--- LampUIKit/LampUIKit/Core/ReviewDetail/ReviewDetailViewModel.swift
+++ LampUIKit/LampUIKit/Core/ReviewDetail/ReviewDetailViewModel.swift
@@ -7,11 +7,16 @@
 
 import Foundation
 
+struct ReviewDetailData {
+    let photoUrlArray: [String]
+    let content: String
+}
+
 class ReviewDetailViewModel: BaseViewModel {
     
-    private(set) var data: ReviewData
+    private(set) var data: ReviewDetailData
     
-    init(_ data: ReviewData) {
+    init(_ data: ReviewDetailData) {
         self.data = data
         super.init()
     }
