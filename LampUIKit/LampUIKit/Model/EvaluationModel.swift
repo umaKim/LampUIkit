dfe7f9344e85fa1b55aa4f798a41ee3c3367dc90
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-09-26 21:41:26 +0900

    chore: make a file

---
+++ LampUIKit/LampUIKit/Model/EvaluationModel.swift
@@ -0,0 +1,13 @@
+//
+//  EvaluationModel.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/09/26.
+//
+
+import Foundation
+
+struct EvaluationModel {
+    var isSelected: Bool
+    var title: String
+}
