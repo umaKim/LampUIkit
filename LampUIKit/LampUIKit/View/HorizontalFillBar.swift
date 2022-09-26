ebeb8614fd59bb7ae19045c46cf3851d5ee9d450
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-09-26 21:39:34 +0900

    chore: make file

---
+++ LampUIKit/LampUIKit/View/HorizontalFillBar.swift
@@ -0,0 +1,45 @@
+//
+//  HorizontalFillBar.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/09/26.
+//
+
+import UIKit
+
+final class HorizontalFillBar: UIProgressView {
+    private let height: CGFloat
+    
+    init(
+        height: CGFloat,
+        fillerColor: UIColor,
+        trackColor: UIColor
+    ) {
+        self.height = height
+        super.init(frame: .zero)
+        
+        progressViewStyle = .bar
+    
+        progressTintColor = fillerColor
+        trackTintColor = trackColor
+        
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+    
+    public var barColor: UIColor? {
+        didSet {
+            progressTintColor = barColor
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = height / 2
+        clipsToBounds = true
+    }
+}
