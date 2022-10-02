8c7f3451ee832b9c9d644aea0c67e52732c27889
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-10-02 23:46:54 +0900

    chore: delete unnecessary lines

--- LampUIKit/LampUIKit/Core/ReviewDetail/ReviewDetailViewController.swift
+++ LampUIKit/LampUIKit/Core/ReviewDetail/ReviewDetailViewController.swift
@@ -11,33 +11,6 @@ class ReviewDetailViewController: BaseViewController<ReviewDetailView, ReviewDet
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        contentView.collectionView.dataSource = self
-        contentView.collectionView.delegate = self
-        
         contentView.configure(with: viewModel.data)
-    }
-}
-
-extension ReviewDetailViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReviewDetailImageCollectionViewCell.identifier, for: indexPath) as? ReviewDetailImageCollectionViewCell
-        else {return UICollectionViewCell() }
-        cell.configure(with: viewModel.data.photoUrlArray[indexPath.item])
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.data.photoUrlArray.count
-    }
-}
-
-extension ReviewDetailViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        .init(width: UIScreen.main.width, height: UIScreen.main.width)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        0
     }
 }
