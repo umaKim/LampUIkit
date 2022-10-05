869b6d5b7f2dfa865bcf26d7917aab7eccf1ec40
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-10-05 19:42:59 +0900

    chore: declare BaseCollectionViewWithHeader class

---
+++ LampUIKit/LampUIKit/Core/Base/BaseCollectionView.swift
@@ -0,0 +1,33 @@
+//
+//  BaseCollectionView.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/10/04.
+//
+
+import UIKit
+
+protocol Identifiable {
+    static var identifier: String { get }
+}
+
+protocol HeaderCellable: UICollectionReusableView, Identifiable { }
+
+protocol BodyCellable: UICollectionViewCell, Identifiable { }
+
+class BaseCollectionViewWithHeader<HEADERCELL: HeaderCellable, BODYCELL: BodyCellable>: UICollectionView {
+    
+    init(_ scrollDirection: UICollectionView.ScrollDirection = .vertical) {
+        let cl = UICollectionViewFlowLayout()
+        cl.scrollDirection = scrollDirection
+        super.init(frame: .zero, collectionViewLayout: cl)
+        
+        register(HEADERCELL.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HEADERCELL.identifier)
+        register(BODYCELL.self, forCellWithReuseIdentifier: BODYCELL.identifier)
+        backgroundColor = .greyshWhite
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
