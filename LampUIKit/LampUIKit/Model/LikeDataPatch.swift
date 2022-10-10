8c7b31716c843f278c64d45c99d9aab7c6aaa9c5
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-10-10 23:37:08 +0900

    chore: declare LikeDataPatch

---
+++ LampUIKit/LampUIKit/Model/LikeDataPatch.swift
@@ -0,0 +1,13 @@
+//
+//  LikeDataPatch.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/10/09.
+//
+
+import Foundation
+
+struct LikeDataPatch: Codable {
+    let token: String
+    let targetReviewId: String
+}
