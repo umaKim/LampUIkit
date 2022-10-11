d2b97ea1a2df782ec9e3b687a2c34c177e28095d
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-10-11 20:54:24 +0900

    chore: set Empty

---
+++ LampUIKit/LampUIKit/Managers/Empty.swift
@@ -0,0 +1,13 @@
+//
+//  Empty.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/10/11.
+//
+
+import Foundation
+
+public struct Empty: Encodable {
+    /// Static `Empty` instance used for all `Empty` responses.
+    public static let value = Empty()
+}
