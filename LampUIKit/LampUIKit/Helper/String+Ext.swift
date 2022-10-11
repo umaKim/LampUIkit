e5947e278a3da507de2ded3df1b67b2f5669f635
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-10-11 20:52:39 +0900

    chore: add addingPercentEncoding for extension

--- LampUIKit/LampUIKit/Helper/String+Ext.swift
+++ LampUIKit/LampUIKit/Helper/String+Ext.swift
@@ -62,3 +62,9 @@ extension String {
         return localizedText
     }
 }
+
+extension String {
+    var addingPercentEncoding: String {
+        self.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+    }
+}
