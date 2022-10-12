f0504a19db309ddaaca99c84b081cf032fb49a34
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-10-12 19:42:15 +0900

    chore: file for URLRequestable

---
+++ LampUIKit/LampUIKit/Managers/URLRequestable.swift
@@ -0,0 +1,13 @@
+//
+//  URLRequestable.swift
+//  LampUIKit
+//
+//  Created by 김윤석 on 2022/10/09.
+//
+import Alamofire
+import Foundation
+
+protocol URLRequestable {
+  var baseURL: String { get }
+  var endPoint: String { get }
+}
