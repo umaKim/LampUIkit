082fbb67c7084efa248ff266f3d6ec49ef3d82d9
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-10-19 21:45:47 +0900

    chore: set up MockNetwork

---
+++ LampUIKit/LampUIKitTests/Mock/MockNetworkManager.swift
@@ -0,0 +1,43 @@
+//
+//  MockNetworkManager.swift
+//  LampUIKitTests
+//
+//  Created by 김윤석 on 2022/10/19.
+//
+import Alamofire
+import Foundation
+@testable import LampUIKit
+
+class MockNetworkManager: Networkable {
+    var getSuccessCheckUserExist: UserExistCheckResponse?
+    
+    func get<RESPONSE>(_ request: LampUIKit.URLConfigurator, _ response: RESPONSE.Type, completion: @escaping (Result<RESPONSE, Alamofire.AFError>) -> Void) where RESPONSE : Decodable {
+        switch request {
+        case .checkUserExist(let uid):
+            if let getSuccessCheckUserExist = getSuccessCheckUserExist {
+                completion(.success(getSuccessCheckUserExist as! RESPONSE))
+            } else {
+                
+            }
+            
+        default:
+            break
+        }
+    }
+    
+    func post<PARAMETERS, RESPONSE>(_ request: LampUIKit.URLConfigurator, _ parameter: PARAMETERS, _ response: RESPONSE.Type, completion: @escaping (Result<RESPONSE, Alamofire.AFError>) -> Void) where PARAMETERS : Encodable, RESPONSE : Decodable {
+        
+    }
+    
+    func patch<PARAMETERS, RESPONSE>(_ request: LampUIKit.URLConfigurator, _ response: RESPONSE.Type, parameters: PARAMETERS?, completion: @escaping (Result<RESPONSE, Alamofire.AFError>) -> Void) where PARAMETERS : Encodable, RESPONSE : Decodable {
+        
+    }
+    
+    func delete<RESPONSE>(_ request: LampUIKit.URLConfigurator, _ response: RESPONSE.Type, completion: @escaping (Result<RESPONSE, Alamofire.AFError>) -> Void) where RESPONSE : Decodable {
+        
+    }
+    
+    func uploadMultipartForm<RESPONSE>(_ request: LampUIKit.URLConfigurator, _ datum: [Data], _ contentId: String, _ response: RESPONSE.Type, completion: @escaping (Result<RESPONSE, Alamofire.AFError>) -> Void) where RESPONSE : Decodable {
+        
+    }
+}
