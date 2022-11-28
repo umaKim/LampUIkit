74acd7d4736c6b41c015ac38636714a3cbb1a99e
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-11-28 19:27:43 +0900

    chore: refactor MapMarkerType to keep up with OCP

--- LampUIKit/LampUIKit/Enum/MapMarkerType.swift
+++ LampUIKit/LampUIKit/Enum/MapMarkerType.swift
@@ -6,9 +6,33 @@
 //
 
 import Foundation
+import UIKit
 
 enum MapMarkerType {
     case recommended
     case destination
     case completed
 }
+
+protocol MapMaker {
+    var image: UIImage { get }
+}
+
+struct RecommendedMapMarker: MapMaker {
+    var image: UIImage {
+        return .circleRecommended ?? UIImage()
+    }
+}
+
+struct DestinationMapMarker: MapMaker {
+    var image: UIImage {
+        return .circleDestination ?? UIImage()
+    }
+}
+
+struct CompletedMapMarker: MapMaker {
+    var image: UIImage {
+        return .circleCompleted ?? UIImage()
+    }
+}
+
